407ea5e2c9f91fd55b048863273ba4908b0aeec5
JuniorTenorio16 <juniortenorio1016@gmail.com>
2021-04-20 04:58:57 -0300

    change route calculate and request googleapi

--- routes/calculate.js
+++ routes/calculate.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const googleapi = require('../src/service/googleapi.js');
+const calculate = require('../src/helpers/calculate');
 
-router.get('/', (req, res, next) => {
+const calculateRoute = async(req, res) => {
+    const address = req.params.address;
+    response = await googleapi.getGeoLocation(address.replace(' ', '+'));
+    if(response == false) {
+        res.status(501).send({
+            mesagem: 'Problema ao processar a sua requisição.'
+        });
+    } else if(response.length == 1){
+        res.status(400).send({
+            mesagem: 'Sintaxe invalida, necessario adicionar dois ou mais endereços nos parametros.'
+        });
+    }
+    result = await calculate.treeLocation(response);
     res.status(200).send({
-        mesagem: 'Rota de calculo euclidiano'
+        mesagem: result
     });
-});
+}
+router.get('/:address', calculateRoute);
 
 module.exports = router;
---
+++ src/service/googleapi.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+
+const url = "https://maps.googleapis.com";
+
+const path = '/maps/api/geocode/json?';
+const api = axios.create({
+    baseURL: url,
+});
+
+async function getGeoLocation(params){
+   return await axios.get(encodeURI(url+path+params+'&key=AIzaSyDbDgzohjahyjDF9ryw0Aj6LDoEmVf1fG4'))
+   .then((response) => {
+    const results = response.data.results;
+    let data = [];
+    results.forEach(address => {
+        let location = {};
+        location.address = address.formatted_address
+        location.lat = address.geometry.location.lat;
+        location.lng = address.geometry.location.lng;
+        data.push(location);
+    });
+    return data;
+   })
+   .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+        return false;
+   })
+}
+
+module.exports = { getGeoLocation };
